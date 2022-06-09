3fa53764488d154ba349e285722a2e8230c8e812
Sohom Mondal <79823137+its-Sohom02@users.noreply.github.com>
2022-06-09 23:16:07 +0530

    Time: 0 ms (100.00%), Space: 5.7 MB (98.42%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
+    int w(int n,int k)
+    {
+        if(n==1) return 0;
+        
+        return (w(n-1,k)+k)%n;
+    }
     int findTheWinner(int n, int k) {
-        if(n==1)
-            return 1;
-        int x = findTheWinner(n-1,x);
-        int y = (x+k-1)%n+1;
-        return y;
+        // if(n==1)
+        //     return 1;
+        // int x = findTheWinner(n-1,x);
+        // int y = (x+k-1)%n+1;
+        // return y;
+        
+        return w(n,k)+1;
     }
 };
